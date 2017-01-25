4c10740bb064b49e0bf6d37c0f0404efe4fc1768
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2017-01-25 14:45:27 +0100

    Google Webmastertools Authentifizierung

--- web/app/themes/wolfzeit/templates/head.php
+++ web/app/themes/wolfzeit/templates/head.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="google-site-verification" content="rB1u-C5tblsRG9CgAgF9TwyNZDhC2KdS9Q8uYipghuE" />
   <?php wp_head(); ?>
 </head>
